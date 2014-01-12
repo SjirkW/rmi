ca1cb69cb3beccdab284234fd7d9ccae5e654e89
glowingmonx <witt0800@tech.nhl.nl>
2014-01-12 21:05:27 +0100

    Refactoring

---
+++ src/rmi/server/Game.java
@@ -0,0 +1,93 @@
+package rmi.server;
+
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+import rmi.client.ClientInterface;
+import rmi.enums.Enums.Direction;
+
+public class Game {
+	protected ArrayList<Player> players = new ArrayList<Player>();
+
+	public Game() {
+
+	}
+	
+	public void addPlayer(Player player) throws RemoteException{
+		players.add(player);
+		broadcastMessage("--> " + player.getName() + " is entering the game", "");
+	}
+	
+	public void move(ClientInterface client, Direction direction)
+			throws RemoteException {
+		Player player = getPlayer(client);
+		player.changePosition(direction);
+		if (isColliding(player)) {
+			player.incScore();
+			System.out.println(player.getName() + " Scored! (" + player.getScore() + ")" );
+		}
+	}
+
+	public boolean isColliding(Player player) throws RemoteException {
+		for (int i = 0; i < players.size(); i++) {
+			Player otherPlayer = players.get(i);
+			if (!player.getClient().equals(otherPlayer.getClient())){
+				if (otherPlayer.hasPosition(player.getX(), player.getY())) {
+					respawn(otherPlayer);
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	public void respawn(Player player){
+		//get new coords
+		player.respawn();
+		for (int i = 0; i < players.size(); i++) {	
+			Player otherPlayer = players.get(i);
+			//dont compare if it's the same player
+			if (!player.getClient().equals(otherPlayer.getClient())){
+				if ((player.getX() == otherPlayer.getX())
+						&& player.getY() == otherPlayer.getY()){
+					//recursive call
+					respawn(player);
+				}
+			}
+		}
+		System.out.println(player.getName() + " has spawned at " + player.getX() + "," + player.getY());
+	}
+	
+	public void remove(ClientInterface client){
+		players.remove(getPlayer(client));
+	}
+	
+	public void broadcastMessage(String message, String nickname)
+			throws RemoteException {
+		for (int i = 0; i < players.size(); i++) {
+			ClientInterface c = players.get(i).getClient();
+			try {
+				c.getMessage(message, nickname);
+			} catch (RemoteException e) {
+				logout(c);
+				i = i - 1;
+			}
+		}
+	}
+	
+	private void logout(ClientInterface client) throws RemoteException {
+		broadcastMessage("--> " + "someone" + " left the game", "");
+		remove(client);
+	}
+	
+	private Player getPlayer(ClientInterface client) {
+		for (int i = 0; i < players.size(); i++) {
+			Player player = players.get(i);
+			if (player.getClient().equals(client)) {
+				return player;
+			}
+		}
+		return null;
+	}
+	
+}
--- src/rmi/server/Server.java
+++ src/rmi/server/Server.java
@@ -12,46 +12,14 @@ import rmi.enums.Enums.Direction;
 public class Server extends UnicastRemoteObject implements ServerInterface {
 
 	protected ArrayList<ClientInterface> clients = new ArrayList<ClientInterface>();
-	protected ArrayList<Player> players = new ArrayList<Player>();
+	Game game;
 
 	public static final int GRID_SIZE = 10;
 
 	public Server() throws RemoteException {
+		game = new Game();
 	}
-
-	public void login(ClientInterface client, String nickname)
-			throws RemoteException {
-		broadcastMessage("--> " + nickname + " is entering the game", "");
-		Player player = new Player(client, nickname);
-		players.add(player);
-	}
-
-	public void logout(ClientInterface client) throws RemoteException {
-		broadcastMessage("--> " + "someone" + " left the game", "");
-
-		// for (int i = 0; i < players.size(); i++) {
-		// Player player = players.get(i);
-		// if (player.getClient().equals(client)) {
-		// players.remove(i);
-		// }
-		// }
-
-		players.remove(getPlayer(client));
-	}
-
-	public void broadcastMessage(String message, String nickname)
-			throws RemoteException {
-		for (int i = 0; i < clients.size(); i++) {
-			ClientInterface c = clients.get(i);
-			try {
-				c.getMessage(message, nickname);
-			} catch (RemoteException e) {
-				logout(c);
-				i = i - 1;
-			}
-		}
-	}
-
+	
 	public static void main(String[] args) {
 		try {
 			LocateRegistry.createRegistry(1099);
@@ -62,54 +30,23 @@ public class Server extends UnicastRemoteObject implements ServerInterface {
 		}
 	}
 
+	public void login(ClientInterface client, String nickname)
+			throws RemoteException {
+		Player player = new Player(client, nickname);
+		game.addPlayer(player);
+	}
+
+	public void broadcastMessage(String message, String nickname)
+			throws RemoteException {
+		game.broadcastMessage(message, nickname);
+	}
+
+
+
 	@Override
 	public void move(ClientInterface client, Direction direction)
 			throws RemoteException {
-		Player player = getPlayer(client);
-		player.changePosition(direction);
-		if (isColliding(player)) {
-			player.incScore();
-			System.out.println(player.getName() + " Scored! (" + player.getScore() + ")" );
-		}
+		game.move(client, direction);
 	}
 
-	private boolean isColliding(Player player) throws RemoteException {
-		for (int i = 0; i < players.size(); i++) {
-			Player otherPlayer = players.get(i);
-			if (!player.getClient().equals(otherPlayer.getClient())){
-				if (otherPlayer.hasPosition(player.getX(), player.getY())) {
-					respawn(otherPlayer);
-					return true;
-				}
-			}
-		}
-		return false;
-	}
-	
-	private void respawn(Player player){
-		//get new coords
-		player.respawn();
-		for (int i = 0; i < players.size(); i++) {	
-			Player otherPlayer = players.get(i);
-			//dont compare if it's the same player
-			if (!player.getClient().equals(otherPlayer.getClient())){
-				if ((player.getX() == otherPlayer.getX())
-						&& player.getY() == otherPlayer.getY()){
-					//recursive call
-					respawn(player);
-				}
-			}
-		}
-		System.out.println(player.getName() + " has spawned at " + player.getX() + "," + player.getY());
-	}
-
-	private Player getPlayer(ClientInterface client) {
-		for (int i = 0; i < players.size(); i++) {
-			Player player = players.get(i);
-			if (player.getClient().equals(client)) {
-				return player;
-			}
-		}
-		return null;
-	}
 }
